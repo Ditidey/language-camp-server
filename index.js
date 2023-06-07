934218e8acf2c1cd2dff3c9bc9e2deede8a2ca05
Ditidey <ditidey2017@gmail.com>
2023-06-07 21:17:20 +0100

    middle ware added

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req, res) =>{
+    res.send('global talk is talking')
+})
+app.listen(port, () =>{
+    console.log(`global talk running on ${port}`)
+})
