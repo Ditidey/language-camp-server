68292a3ed75fe57c65bb2a41b1b8cfc7d5e11652
Ditidey <ditidey2017@gmail.com>
2023-06-09 10:48:41 +0100

    put for classes added

--- index.js
+++ index.js
@@ -19,66 +19,77 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-     
+
     // await client.connect();
     const classCollections = client.db('global-language').collection('classes')
     const studentCollections = client.db('global-language').collection('students');
 
-    app.put('/students/:email', async(req,res)=>{
-        const email = req.params.email;
-        const info = req.body;
-        const query = {email: email};
-        const options = {upsert: true};
-        const updateInfo = {
-            $set:  info
-        }
-        const result = await studentCollections.updateOne(query,updateInfo,options)
-        res.send(result)
-        
+    app.put('/students/:email', async (req, res) => {
+      const email = req.params.email;
+      const info = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
+      const updateInfo = {
+        $set: info
+      }
+      const result = await studentCollections.updateOne(query, updateInfo, options)
+      res.send(result)
+
     })
-    app.get('/students', async(req, res)=>{
-        const result = await studentCollections.find().toArray();
-        res.send(result)
+    app.get('/students', async (req, res) => {
+      const result = await studentCollections.find().toArray();
+      res.send(result)
     })
-    app.get('/students/:email', async(req, res)=>{
-        const email = req.params.email;
-        // console.log(email)
-        const query = {email: email};
-        const result = await studentCollections.findOne(query);
-        res.send(result);
+    app.get('/students/:email', async (req, res) => {
+      const email = req.params.email;
+      // console.log(email)
+      const query = { email: email };
+      const result = await studentCollections.findOne(query);
+      res.send(result);
     })
 
-    app.post('/add-classes', async (req, res)=>{
-        const eachClass = req.body;
-        const result = await classCollections.insertOne(eachClass)
-        res.send(result);
+    app.post('/add-classes', async (req, res) => {
+      const eachClass = req.body;
+      const result = await classCollections.insertOne(eachClass)
+      res.send(result);
     })
-    app.get('/classes', async(req, res)=>{
-        const result = await classCollections.find().toArray();
-        res.send(result)
+    app.get('/classes', async (req, res) => {
+      const query = {};
+      // if (req.query?.email) {
+      //   query = { email: req.query.email }
+      // }
+      const result = await classCollections.find(query).toArray();
+      res.send(result)
     })
-    app.get('/classes/:email', async(req, res)=>{
-       const email = req.params.email;
-       const query = {email: email};
-        const result = await classCollections.find(query).toArray();
-        res.send(result)
+    
+    app.patch('/classes/:id', async (req, res) => {
+      const id = req.params.id;
+      const infoRole = req.body;
+      // console.log(infoRole)
+      const query = { _id: new ObjectId(id) };
+      const updateClass = {
+        $set: { status: infoRole.status }
+      }
+      const result = await classCollections.updateOne(query, updateClass);
+      res.send(result)
     })
-    app.patch('/classes/:id', async(req,res)=>{
-        const id = req.params.id;
-        const infoRole = req.body;
-        // console.log(infoRole)
-        const query = {_id: new ObjectId(id)};
-        const updateClass = {
-            $set: {status: infoRole.status}
-        }
-        const result = await classCollections.updateOne(query, updateClass);
-        res.send(result)
+    app.put('/classes/:id', async (req, res) => {
+      const id = req.params.id;
+      const infoRole = req.body;
+      // console.log(infoRole)
+      const query = { _id: new ObjectId(id) };
+      const updateClass = {
+        $set: { students: infoRole.students }
+      }
+      const options = { upsert: true };
+      const result = await classCollections.updateOne(query, updateClass, options);
+      res.send(result)
     })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-     
+
     // await client.close();
   }
 }
@@ -86,9 +97,9 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res) =>{
-    res.send('global talk is talking')
+app.get('/', (req, res) => {
+  res.send('global talk is talking')
 })
-app.listen(port, () =>{
-    console.log(`global talk running on ${port}`)
+app.listen(port, () => {
+  console.log(`global talk running on ${port}`)
 })
